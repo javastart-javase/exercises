526fe940ebbd202518d8ee9b1f7601aa27632053
Lipien <adam@pawtel.pl>
2019-11-07 07:40:55 +0100

    Datatypes first commit.

---
+++ src/main/pl/pawtel/datatypes/DataTypes.java
@@ -0,0 +1,15 @@
+package main.pl.pawtel.datatypes;
+
+/*
+* Popraw poniższy kod w taki sposób, aby na ekranie został wyświetlony
+* przedstawiony wynik (2.5).
+* */
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 4;
+        int div = x / y;
+        System.out.println(div);
+    }
+}
