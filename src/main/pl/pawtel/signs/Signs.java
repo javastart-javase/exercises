6aa149fbf5dcd751e014611cab043f93a04272f9
Lipien <adam@pawtel.pl>
2019-11-10 10:46:24 +0100

    Characters in unicode.

---
+++ src/main/pl/pawtel/signs/Signs.java
@@ -0,0 +1,12 @@
+package main.pl.pawtel.signs;
+
+public class Signs {
+    public static void main(String[] args) {
+        char k = 0x004B;
+        char o = 0x004F;
+        char t = 0x0054;
+        System.out.print(k);
+        System.out.print(o);
+        System.out.print(t);
+    }
+}
