caaabdc58c9acb2e52fc0482eff3190721a10900
Lipien <adam@pawtel.pl>
2019-11-11 14:09:17 +0100

    Cabrio: roof opener.

---
+++ src/main/pl/pawtel/cabrio/CabioApp.java
@@ -0,0 +1,8 @@
+package main.pl.pawtel.cabrio;
+
+public class CabioApp {
+    public static void main(String[] args) {
+        Cabrio cabrio = new Cabrio("Volvo", "v40", false, false);
+        cabrio.roofOpener();
+    }
+}
---
+++ src/main/pl/pawtel/cabrio/Cabrio.java
@@ -0,0 +1,61 @@
+package main.pl.pawtel.cabrio;
+
+public class Cabrio {
+
+    private String brand;
+    private String manufacturer;
+    private boolean isMoving;
+    private boolean isRoofOpen;
+
+    public Cabrio(String brand, String manufacturer, boolean isMoving, boolean isRoofOpen) {
+        this.brand = brand;
+        this.manufacturer = manufacturer;
+        this.isMoving = isMoving;
+        this.isRoofOpen = isRoofOpen;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public void setMoving(boolean moving) {
+        isMoving = moving;
+    }
+
+    public boolean isRoofOpen() {
+        return isRoofOpen;
+    }
+
+    public void setRoofOpen(boolean roofOpen) {
+        isRoofOpen = roofOpen;
+    }
+
+    public void roofOpener() {
+        if (isMoving == true) {
+            System.out.println("Car is moving: you can't open/close roof");
+        } else
+            System.out.println("Openin/closing roof");
+        {
+            if (isRoofOpen == true) {
+                isRoofOpen = false;
+            } else
+                isRoofOpen = true;
+        }
+    }
+}
