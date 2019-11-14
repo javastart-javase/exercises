613c50d2e60008bd1eefe7bce11b1a69a0a66758
Lipien <adam@pawtel.pl>
2019-11-14 16:59:31 +0100

    Refactor of methods to static.

--- src/main/pl/pawtel/incomecalculator/PaymentCalculator.java
+++ src/main/pl/pawtel/incomecalculator/PaymentCalculator.java
@@ -4,11 +4,11 @@ public class PaymentCalculator {
 
     Employee employee;
 
-    public double yearlyPaymentNetto(Employee employee) {
+    public static double yearlyPaymentNetto(Employee employee) {
         return employee.getMonthlySalary() * 12;
     }
 
-    public double yearlyPaymentBrutto(Employee employee) {
+    public static double yearlyPaymentBrutto(Employee employee) {
         double zus = 12 * 1000;
         double tax = 0.2 * employee.getMonthlySalary();
         return zus + (12 * (employee.getMonthlySalary() + tax));
--- src/main/pl/pawtel/incomecalculator/TestingApp.java
+++ src/main/pl/pawtel/incomecalculator/TestingApp.java
@@ -3,9 +3,8 @@ package main.pl.pawtel.incomecalculator;
 public class TestingApp {
     public static void main(String[] args) {
         Employee employee = new Employee("Adam", "Nowak", 3000);
-        PaymentCalculator paymentCalculator = new PaymentCalculator();
-        double netto = paymentCalculator.yearlyPaymentNetto(employee);
-        double brutto = paymentCalculator.yearlyPaymentBrutto(employee);
+        double netto = PaymentCalculator.yearlyPaymentNetto(employee);
+        double brutto = PaymentCalculator.yearlyPaymentBrutto(employee);
         System.out.println(netto);
         System.out.println(brutto);
     }
